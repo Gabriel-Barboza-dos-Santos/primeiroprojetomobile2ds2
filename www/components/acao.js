11c1665f361b7461b7ca0bf629f36acee72a2eed
Gabriel-Barboza-dos-Santos <newtoneletricga@gmail.com>
2019-07-26 03:21:21 +0900

    calculando, multiplicando e somando ta bê rsrsrsrsrsrsrsrsrs

--- www/components/acao.js
+++ www/components/acao.js
@@ -5,3 +5,19 @@ $(document).on("click","#somar",function(){
   var result = parseFloat(valor1) + parseFloat(valor2);
   $("#resultado").val(result);
 });
+
+// This is a JavaScript file
+$(document).on("click","#subtrair",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var result = parseFloat(valor1) - parseFloat(valor2);
+  $("#resultado1").val(result);
+});
+
+// This is a JavaScript file
+$(document).on("click","#multi",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var result = parseFloat(valor1) * parseFloat(valor2);
+  $("#resultado2").val(result);
+});
